d85d6aec7c16560d55b583e128083705c26a874a
Dan Cornutt <dacornutt@gmail.com>
2019-04-28 08:21:20 -0700

    Tests pass, cleaned code up

--- calculator.py
+++ calculator.py
@@ -1,42 +1,15 @@
 #!/usr/bin/env python
 
-import pprint
 
 """
-For your homework this week, you'll be creating a wsgi application of
-your own.
-
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
+wsgi web application for calculations
+supporting the following:
 
   * Addition
   * Subtractions
   * Multiplication
   * Division
 
-Your users should be able to send appropriate requests and get back
-proper responses. For example, if I open a browser to your wsgi
-application at `http://localhost:8080/multiple/3/5' then the response
-body in my browser should be `15`.
-
-Consider the following URL/Response body pairs as tests:
-
-```
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
-```
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
   * I should also be able to see a home page (http://localhost:8080/)
     that explains how to perform calculations.
   * Commit and push your changes to your fork.
@@ -48,14 +21,26 @@ To submit your homework:
 
 def home():
     """Returns body of web page for Home page"""
-    return """Welcome to the home page!
-    this groudbreaking calculator accepts the following operations:
-        'add' for addition
-        'subract' for subtraction
-        'multiply' for multiplication
-        'divide' for division
+    return """<h1>Welcome to the Calculator Home Page!</h1>
+    <body>
+        <h3>This groudbreaking calculator accepts the following operations:</h3>
+        <ul>
+            <li>'add' for addition</li>
+            <li>'subract' for subtraction</li>
+            <li>'multiply' for multiplication</li>
+            <li>'divide' for division</li>
+        </ul>
+    </body>
+    """
 
 
+def no_operator(*args):
+    """Returns body of web page for no operation found"""
+    return """
+    <body>
+        <h3>Sorry, your operation was not found.</h3>
+        Please try another operation
+    </body>
     """
 
 
@@ -100,7 +85,7 @@ def dispatch(f_name):
         "divide": divide,
         "": home,
     }
-    func = dispatch_d.get(f_name)
+    func = dispatch_d.get(f_name, no_operator)
     return func
 
 
@@ -110,37 +95,20 @@ def resolve_path(path):
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-
-    # args = ['25', '32']
     chunks = path.split("/")
-    print("function chunk is: {}".format(chunks[1]))
     func = dispatch(chunks[1])
     args = [float(x) for x in chunks[2:]]
     return func, args
 
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    # pprint.pprint(environ)
-    print("In App func")
     headers = [('Content-type', 'text/html')]
     try:
-        print("In try loop")
         path = environ.get('PATH_INFO', None)
         if path is None:
-            print("path is None! path={}".format(path))
             raise NameError
-        print("Path ISNT None")
         func, args = resolve_path(path)
         body = func(*args)
-        print("body= {}".format(body))
         status = "200 OK"
     except NameError:
         status = "404 Not Found"
@@ -155,8 +123,6 @@ def application(environ, start_response):
 
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
     from wsgiref.simple_server import make_server
     srv = make_server('localhost', 8080, application)
     srv.serve_forever()
